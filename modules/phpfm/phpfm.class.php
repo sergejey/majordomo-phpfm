1edb2db8bb9dbe2140ffcd6ba5fee685e44c46c8
Sergei Jeihala <jey@tut.by>
2016-12-23 13:32:54 +0300

    Initial commit

---
+++ modules/phpfm/phpfm.class.php
@@ -0,0 +1,164 @@
+<?php
+/**
+* File Manager 
+* @package project
+* @author Wizard <sergejey@gmail.com>
+* @copyright http://majordomo.smartliving.ru/
+* @version 0.1 (wizard, 13:12:18 [Dec 23, 2016])
+*/
+//
+//
+class phpfm extends module {
+/**
+* phpfm
+*
+* Module class constructor
+*
+* @access private
+*/
+function phpfm() {
+  $this->name="phpfm";
+  $this->title="File Manager";
+  $this->module_category="<#LANG_SECTION_APPLICATIONS#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams($data=0) {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams() {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+ $this->getConfig();
+ $out['API_URL']=$this->config['API_URL'];
+ if (!$out['API_URL']) {
+  $out['API_URL']='http://';
+ }
+ $out['API_KEY']=$this->config['API_KEY'];
+ $out['API_USERNAME']=$this->config['API_USERNAME'];
+ $out['API_PASSWORD']=$this->config['API_PASSWORD'];
+ if ($this->view_mode=='update_settings') {
+   global $api_url;
+   $this->config['API_URL']=$api_url;
+   global $api_key;
+   $this->config['API_KEY']=$api_key;
+   global $api_username;
+   $this->config['API_USERNAME']=$api_username;
+   global $api_password;
+   $this->config['API_PASSWORD']=$api_password;
+   $this->saveConfig();
+   $this->redirect("?");
+ }
+}
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ if ($this->owner->action=='apps') {
+  $this->redirect(ROOTHTML."modules/phpfm/index.php");
+ }
+ $this->admin($out);
+}
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($data='') {
+  parent::install();
+ }
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgRGVjIDIzLCAyMDE2IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
